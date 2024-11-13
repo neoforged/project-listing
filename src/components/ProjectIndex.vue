ba52b6d7ab6b809e6498e1287852dcbf8d32463b
rambo0247 <60529713+rambo0247@users.noreply.github.com>
2024-11-13 09:05:28 -0500

    Add latest version to the home page cards for each project (#11)

--- src/components/ProjectIndex.vue
+++ src/components/ProjectIndex.vue
@@ -1,26 +1,30 @@
 <template>
   <v-container class="fill-height">
     <v-responsive class="align-center fill-height">
-      <v-img height="150" src="https://github.com/Neoforged.png"/>
+      <v-img height="150" src="https://github.com/Neoforged.png" />
       <h1 class="text-center">NeoForged Project Index</h1>
 
-      <div class="py-6"/>
+      <div class="py-6" />
 
       <v-row class="d-flex align-center justify-center">
-        <v-col v-for="repo in Object.keys(data)" :key="repo" cols="12" sm="5">
-          <v-card class="mx-auto" :title="data[repo].name" :href="`/${repo}`">
+        <v-col v-for="repo in repos" :key="repo.projectRepo" cols="12" sm="5">
+          <v-card class="mx-auto" :title="repo.name" :href="`/${repo.projectRepo}`">
             <template v-slot:append>
               <Suspense>
-                <RepoChips :repo="repo" />
+                <RepoChips :repo="repo.projectRepo" />
               </Suspense>
             </template>
+
             <v-card-item>
               <Suspense>
-                <RepoDescription :repo="repo" />
+                <RepoDescription :repo="repo.projectRepo" />
               </Suspense>
             </v-card-item>
+
             <v-card-actions>
-              <v-btn :href="`/${repo}`">Details</v-btn>
+              <v-btn :href="`/${repo.projectRepo}`">Details</v-btn>
+              <v-spacer v-if="repo.latestVersion"></v-spacer>
+              <p :href="`/${repo.projectRepo}`" v-if="repo.latestVersion">{{ repo.latestVersion }}</p>
             </v-card-actions>
           </v-card>
         </v-col>
@@ -33,13 +37,41 @@
 import json from "../../src/repos.json";
 import RepoChips from "@/components/project/RepoChips.vue";
 import RepoDescription from "@/components/project/RepoDescription.vue";
+import { ref, onMounted } from "vue";
 
 export default {
   components: {RepoChips, RepoDescription},
-  data() {
+  async setup() {
+    const repos = ref([])
+
+    const fetchRepoData = async () => {
+      const repoDataPromises = Object.keys(json).map(async (project) => {
+        const projectInfo = json[project];
+        const mavenPath = projectInfo.artifact.replace(/\./g, '/').replace(':', '/');
+
+        const latestVersion = await fetch(`https://maven.neoforged.net/api/maven/latest/version/releases/${mavenPath}`)
+          .then((res) => res.json())
+          .then((res) => res.version)
+          .catch((err) => {
+            console.error("Failed to fetch versions:", err);
+            return [];
+          });
+
+        return {
+          ...projectInfo,
+          projectRepo: project,
+          latestVersion: latestVersion || '',
+        };
+      });
+
+      repos.value = await Promise.all(repoDataPromises);
+    };
+
+    onMounted(fetchRepoData);
+
     return {
-      data: json
+      repos
     }
-  },
+  }
 };
 </script>
